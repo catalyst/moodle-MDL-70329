087a0a6a7e02ff3f20a5e2556686e702150b9767
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-04-23 18:05:08 +1000

    Issue #21: New question bank database schema - update upgrade.php

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2560,5 +2560,186 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2021052500.78);
     }
 
+    if ($oldversion < 2021052500.82) {
+        // Define table question_bank_entry to be created.
+        $table = new xmldb_table('question_bank_entry');
+
+        // Adding fields to table question_bank_entry.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('questioncategoryid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('idnumber', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+        $table->add_field('hidden', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('ownerid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table question_bank_entry.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('questioncategoryid', XMLDB_KEY_FOREIGN, ['questioncategoryid'], 'question_categories', ['id']);
+        $table->add_key('ownerid', XMLDB_KEY_FOREIGN, ['ownerid'], 'user', ['id']);
+
+        // Adding indexes to table question_bank_entry.
+        $table->add_index('categoryidnumber', XMLDB_INDEX_UNIQUE, ['questioncategoryid', 'idnumber']);
+
+        // Conditionally launch create table for question_bank_entry.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table question_versions to be created.
+        $table = new xmldb_table('question_versions');
+
+        // Adding fields to table question_versions.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('questionbankentryid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('versionnumber', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 1);
+        $table->add_field('questionid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+
+        // Adding keys to table question_versions.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('questionbankentryid', XMLDB_KEY_FOREIGN, ['questionbankentryid'], 'question_bank_entry', ['id']);
+        $table->add_key('questionid', XMLDB_KEY_FOREIGN, ['questionid'], 'question', ['id']);
+
+        // Conditionally launch create table for question_versions.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table question_references to be created.
+        $table = new xmldb_table('question_references');
+
+        // Adding fields to table question_references.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('usingcontextid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('component', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+        $table->add_field('questionarea', XMLDB_TYPE_CHAR, '50', null, null, null, null);
+        $table->add_field('itemid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('questionbankentryid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('versionnumber', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table question_references.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('usingcontextid', XMLDB_KEY_FOREIGN, ['usingcontextid'], 'context', ['id']);
+        $table->add_key('itemid', XMLDB_KEY_FOREIGN, ['itemid'], 'quiz_slots', ['id']);
+        $table->add_key('questionbankentryid', XMLDB_KEY_FOREIGN, ['questionbankentryid'], 'question_bank_entry', ['id']);
+
+        // Conditionally launch create table for question_references.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table question_set_references to be created.
+        $table = new xmldb_table('question_set_references');
+
+        // Adding fields to table question_set_references.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('usingcontextid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('component', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+        $table->add_field('questionarea', XMLDB_TYPE_CHAR, '50', null, null, null, null);
+        $table->add_field('itemid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('questionscontextid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('filtercondition', XMLDB_TYPE_CHAR, '1333', null, null, null, null);
+
+        // Adding keys to table question_set_references.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('usingcontextid', XMLDB_KEY_FOREIGN, ['usingcontextid'], 'context', ['id']);
+        $table->add_key('itemid', XMLDB_KEY_FOREIGN, ['itemid'], 'quiz_slots', ['id']);
+        $table->add_key('questionscontextid', XMLDB_KEY_FOREIGN, ['questionscontextid'], 'context', ['id']);
+
+        // Conditionally launch create table for question_set_references.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Split question table in two tables (question_bank_entry and question_versions).
+        $questions = $DB->get_records('question');
+        foreach ($questions as $question) {
+            $questionbankentry = new \stdClass();
+            $questionbankentry->questioncategoryid = $question->category;
+            $questionbankentry->name = $question->name;
+            $questionbankentry->idnumber = $question->idnumber;
+            $questionbankentry->hidden = $question->hidden;
+            $questionbankentry->ownerid = $question->createdby;
+            $questionbankentry->id = $DB->insert_record('question_bank_entry', $questionbankentry);
+
+            $questionversions = new \stdClass();
+            $questionversions->questionbankentryid = $questionbankentry->id;
+            $questionversions->versionnumber = 1;
+            $questionversions->questionid = $question->id;
+            $DB->insert_record('question_versions', $questionversions);
+        }
+
+        // Define key, index and fields to be dropped from question.
+        $table = new xmldb_table('question');
+
+        $index = new xmldb_index('categoryidnumber', XMLDB_INDEX_NOTUNIQUE, ['category', 'idnumber']);
+
+        // Conditionally launch drop index categoryidnumber.
+        if ($dbman->index_exists($table, $index)) {
+            $dbman->drop_index($table, $index);
+        }
+
+        // Launch drop key category.
+        $key = new xmldb_key('category', XMLDB_KEY_FOREIGN, ['category'], 'question_categories', ['id']);
+        $dbman->drop_key($table, $key);
+
+        // Launch drop key modifiedby.
+        $key = new xmldb_key('modifiedby', XMLDB_KEY_FOREIGN, ['modifiedby'], 'user', ['id']);
+        $dbman->drop_key($table, $key);
+
+        // Conditionally launch drop field category.
+        $field = new xmldb_field('category');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field name.
+        $field = new xmldb_field('name');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field stamp.
+        $field = new xmldb_field('stamp');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field version.
+        $field = new xmldb_field('version');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field hidden.
+        $field = new xmldb_field('hidden');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field timemodified.
+        $field = new xmldb_field('timemodified');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch drop field idnumber.
+        $field = new xmldb_field('idnumber');
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Conditionally launch rename field modifiedby.
+        $field = new xmldb_field('modifiedby', XMLDB_TYPE_INTEGER, '10', null, null, null);
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->rename_field($table, $field, 'modifierid');
+
+            // Add a foreign key that was dropped before to rename.
+            $table->add_key('modifierid', XMLDB_KEY_FOREIGN, ['modifierid'], 'user', ['id']);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2021052500.82);
+    }
+
     return true;
 }
