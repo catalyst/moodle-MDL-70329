7f59e3c84d07ebc9d8afefcb6b29841025a7dc83
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-05-12 09:50:51 +1000

    Issue #57: Move export_form to classes/form

--- question/bank/exportquestions/export.php
+++ question/bank/exportquestions/export.php
@@ -26,7 +26,7 @@
 
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->dirroot . '/question/editlib.php');
-require_once($CFG->dirroot . '/question/bank/exportquestions/export_form.php');
+require_once($CFG->dirroot . '/question/bank/exportquestions/classes/form/export_form.php');
 require_once($CFG->dirroot . '/question/format.php');
 require_once($CFG->dirroot . '/question/renderer.php');
 
