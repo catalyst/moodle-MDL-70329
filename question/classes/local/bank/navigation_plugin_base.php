49da82ef55e31613dfaaa67bb41be26a82f22352
Safat Shahin <safatshahin@catalyst-au.net>
2021-05-11 10:49:00 +1000

    MDL-71516 core_question: Create new plugin type - qbank
    This change will introduce a new "Question bank" plugin type,
    with the `qbank_` prefix. It will also refactor core code
    to provide a straightforward interface to create plugins
    that provide question bank functionality. This means
    contributors can make their own qbank_* plugins to
    introduce new question bank functionality.
    
    Existing question bank features will not be replaced
    by this tracker, and will still continue to function.
    Each individual qbank_* plugin tracker issue will
    both introduce an existing core feature as a plugin
    and replace the current core functionality.

---
+++ question/classes/local/bank/navigation_plugin_base.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Base class class for column navigation.
+ *
+ * Every qbank plugin wants to implement a navigation node, must extent this class.
+ *
+ * @package    core_question
+ * @copyright  2021 Catalyst IT Australia Pty Ltd
+ * @author     Safat Shahin <safatshahin@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_question\local\bank;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class navigation_plugin_base is the base class for navigation node.
+ *
+ * @package    core_question
+ * @copyright  2021 Catalyst IT Australia Pty Ltd
+ * @author     Safat Shahin <safatshahin@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class navigation_plugin_base {
+
+    /**
+     * Title for this node.
+     */
+    abstract public function get_title();
+
+    /**
+     * Key for this node.
+     */
+    abstract public function get_navigation_key();
+
+    /**
+     * URL for this node
+     */
+    abstract public function get_url();
+
+}
+
