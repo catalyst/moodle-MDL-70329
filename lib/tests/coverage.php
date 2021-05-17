4519e2fbebc8b78a2304521109825dc2601ffdbe
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-05-17 08:15:19 -0400

    Revert "attempt to solve class not found error / bypasses for the moment"
    This reverts commit 5b027762c5766b7e4315797948b976c332aacae3.

--- lib/tests/coverage.php
+++ lib/tests/coverage.php
@@ -35,8 +35,6 @@ return new class extends phpunit_coverage_info {
     /** @var array The list of folders relative to the plugin root to include in coverage generation. */
     protected $includelistfolders = [
         'classes',
-        'xmldb',
-        'weblib.php',
 
         // This is a legacy hangup which relates to parts of the file storage API being placed in the wrong location.
         'filestorage',
@@ -48,11 +46,8 @@ return new class extends phpunit_coverage_info {
     /** @var array The list of folders relative to the plugin root to exclude from coverage generation. */
     protected $excludelistfolders = [
         'filestorage/tests',
-        'classes/access',
     ];
 
     /** @var array The list of files relative to the plugin root to exclude from coverage generation. */
-    protected $excludelistfiles = [
-        'xmldb/xmldb_constants.php',
-    ];
+    protected $excludelistfiles = [];
 };
