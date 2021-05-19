10b5d6c7511d81f7c568cf79164cd4adac8c6d31
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-05-19 10:45:44 -0400

    Issue #90: New query - in progress

--- mod/qbank/db/install.php
+++ mod/qbank/db/install.php
@@ -31,6 +31,7 @@ require_once($CFG->dirroot.'/course/lib.php');
 /**
  * Custom code to be run on installing the plugin.
  * 
+ * @package     mod_qbank
  * @author      Ghaly Marc-Alexandre <marc-alexandreghaly@catalyst-ca.net>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -38,14 +39,24 @@ function xmldb_qbank_install() {
     global $DB;
 
     // Retrieve question bank id that are populated.
-    $sqlq = 'SELECT id, name FROM mdl_question_categories qc WHERE qc.infoformat <> 0';
+    $sqlq = 'SELECT DISTINCT q.category, qc.name, qc.contextid, ctx.contextlevel
+            FROM mdl_question q
+            JOIN mdl_question_categories qc ON qc.id = q.category
+            JOIN mdl_context ctx ON ctx.id = qc.contextid
+            ORDER BY ctx.contextlevel;';
     $rec = $DB->get_records_sql($sqlq);
 
-    $newcourses = array();
+    $populatedcat = array();
     foreach($rec as $cat) {
-        // Fix array append
-        $newcourses[] = $cat->id;
+        // Data from populated questions.
+        $populatedcat[] = $cat->category;
     }
+
+    // Get category list
+    $category = $DB->get_record('course_categories', array('id'=>$categoryid), '*', MUST_EXIST);
+    $catcontext = context_coursecat::instance($category->id);
+
+    $v = 0;
     // Add course here
 
     return true;
