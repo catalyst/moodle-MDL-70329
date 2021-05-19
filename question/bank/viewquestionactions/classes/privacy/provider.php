10a1c2a115c09fcbbb97b15ec00f1053a41db091
Safat Shahin <safatshahin@catalyst-au.net>
2021-05-20 00:26:03 +1000

    typo fix

--- question/bank/viewquestionactions/classes/privacy/provider.php
+++ question/bank/viewquestionactions/classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace qbank_previewquestion\privacy;
+namespace qbank_viewquestionactions\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
