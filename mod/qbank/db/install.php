4407ac0307b591a0e176c54e16e62d1b9ca3fe76
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-05-19 15:03:49 -0400

    Issue #90: Object creation - in progress : category id retrieval

--- mod/qbank/db/install.php
+++ mod/qbank/db/install.php
@@ -38,26 +38,31 @@ require_once($CFG->dirroot.'/course/lib.php');
 function xmldb_qbank_install() {
     global $DB;
 
-    // Retrieve question bank id that are populated.
+    // Retrieve question bank informations that are populated.
     $sqlq = 'SELECT DISTINCT q.category, qc.name, qc.contextid, ctx.contextlevel
             FROM mdl_question q
             JOIN mdl_question_categories qc ON qc.id = q.category
             JOIN mdl_context ctx ON ctx.id = qc.contextid
             ORDER BY ctx.contextlevel;';
+    
     $rec = $DB->get_records_sql($sqlq);
 
+    // Data from populated questions.
     $populatedcat = array();
     foreach($rec as $cat) {
-        // Data from populated questions.
         $populatedcat[] = $cat->category;
+        
+        // Add course here.
+        $newcourse = new stdClass();
+        $newcourse->category    = $cat->category;
+        $newcourse->fullname    = $cat->name;
+        $newcourse->shortname   = $cat->name;
+
+        // Same category id is needed to create course.
+        create_course($newcourse);
+        //$DB->insert_record('course', $newcourse);
     }
 
-    // Get category list
-    $category = $DB->get_record('course_categories', array('id'=>$categoryid), '*', MUST_EXIST);
-    $catcontext = context_coursecat::instance($category->id);
-
-    $v = 0;
-    // Add course here
 
     return true;
 }
