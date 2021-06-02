db32842e20648f7c47cb014618ec011ad27f2624
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-06-02 09:51:18 -0400

    MDL-71613 qbank_viewcreator: Add View creator to core
    This implementation will introduce a qbank plugin "viewcreator"
    which will view the question creator/modifier column in
    the question bank view by replacing the core classes. Having
    this plugin will give users the flexibility of enabling
    or disabling the question creator and modifier column
    in the question bank view.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,8 @@ class core_plugin_manager {
             ),
 
             'qbank' => array(
-                'comment', 'deletequestion', 'exporttoxml', 'tagquestion'
+                'comment', 'deletequestion', 'exporttoxml', 'tagquestion',
+                'viewcreator'
             ),
 
             'qbehaviour' => array(
