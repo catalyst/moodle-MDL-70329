965ba48b8a7eac6f8ea0b0d24c11303ee5acfe6b
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-06-02 16:29:29 +1000

    Add commentall and commentmine capabilities

--- lib/questionlib.php
+++ lib/questionlib.php
@@ -1881,6 +1881,8 @@ function question_get_question_capabilities() {
         'moodle/question:moveall',
         'moodle/question:tagmine',
         'moodle/question:tagall',
+        'moodle/question:commentmine',
+        'moodle/question:commentall',
     );
 }
 
