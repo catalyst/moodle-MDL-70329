91ed08e4e73e7d4938ce7a2295f783739b2d0900
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-06-02 19:16:18 +1000

    MDL-71696 core_question: Implement new database schema
    This commit implements the new database structure for
    versioning in question. It also does the migration of
    current data to the new structure.
    
    Co-Authored-By: Guillermo Gomez Arias <guillermogomez@catalyst-au.net>
    Co-Authored-By: Safat Shahin <safatshahin@catalyst-au.net>

---
+++ question/classes/local/bank/constant.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Constants for the qbank api and questions.
+ *
+ * @package    core_question
+ * @copyright  2021 Catalyst IT Australia Pty Ltd
+ * @author     Safat Shahin <safatshahin@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_question\local\bank;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class constants.
+ *
+ * @package    core_question
+ * @copyright  2021 Catalyst IT Australia Pty Ltd
+ * @author     Safat Shahin <safatshahin@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class constants {
+
+    /**
+     * Const if the question is ready to use.
+     */
+    const QUESTION_STATUS_READY = 0;
+
+    /**
+     * Const if the question is hidden.
+     */
+    const QUESTION_STATUS_HIDDEN = 1;
+
+    /**
+     * const if the question is in draft.
+     */
+    const QUESTION_STATUS_DRAFT = 2;
+}
