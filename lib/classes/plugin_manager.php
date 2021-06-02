48b6a830dcf187fc515bca8fd6f5f503f87ddf88
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-06-02 16:29:29 +1000

    MDL-71642 qbank_comment: Add question comment plugin to core
    This implementation will introduce a qbank plugin
    "comment" which will allow users with the capability
    to comment in a question. It also implements a
    callback for the question preview page to view
    and add comments from the preview page. Comment
    plugin is implemented using the existing comments
    API from the core.
    
    Co-Authored-By: Guillermo Gomez Arias <guillermogomez@catalyst-au.net>

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,7 @@ class core_plugin_manager {
             ),
 
             'qbank' => array(
-                ''
+                'comment',
             ),
 
             'qbehaviour' => array(
