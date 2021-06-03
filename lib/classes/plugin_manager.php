15f626e80ae23a934839cd68b92cdcb7040061a5
Safat Shahin <safatshahin@catalyst-au.net>
2021-06-03 11:28:28 +1000

    MDL-71639 qbank_exporttoxml: Add Export to xml plugin to core
    This implementation will introduce a qbank plugin "exporttoxml"
    which will implement the export to xml action in the question
    bank view by replacing the core class. Having this plugin will
    give users the flexibility of enabling or disabling this action.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,7 @@ class core_plugin_manager {
             ),
 
             'qbank' => [
-                'deletequestion', 'comment', 'previewquestion', 'tagquestion',
+                'comment', 'deletequestion', 'exporttoxml', 'previewquestion', 'tagquestion',
             ],
 
             'qbehaviour' => array(
