b6f7c8852dd5b3007c215a368ab3608d92725a80
Safat Shahin <safatshahin@catalyst-au.net>
2021-06-03 16:20:02 +1000

    add to core

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,7 @@ class core_plugin_manager {
             ),
 
             'qbank' => array(
-                ''
+                'deletequestion',
             ),
 
             'qbehaviour' => array(
