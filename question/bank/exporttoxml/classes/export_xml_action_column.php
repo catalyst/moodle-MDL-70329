03dae667810e6f80e0e08348f9b1e5cea390eb40
Safat Shahin <safatshahin@catalyst-au.net>
2021-06-03 11:28:28 +1000

    MDL-71639 qbank_exporttoxml: Add Export to xml plugin to core
    This implementation will introduce a qbank plugin "exporttoxml"
    which will implement the export to xml action in the question
    bank view by replacing the core class. Having this plugin will
    give users the flexibility of enabling or disabling this action.

--- question/bank/exporttoxml/classes/export_xml_action_column.php
+++ question/bank/exporttoxml/classes/export_xml_action_column.php
@@ -17,36 +17,61 @@
 /**
  * Question bank column export the question in Moodle XML format.
  *
- * @package   core_question
+ * @package   qbank_exporttoxml
  * @copyright 2019 The Open University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace core_question\bank;
+namespace qbank_exporttoxml;
+
 defined('MOODLE_INTERNAL') || die();
 
+use core_question\local\bank\menu_action_column_base;
 
 /**
  * Question bank column export the question in Moodle XML format.
  *
+ * @package   qbank_exporttoxml
  * @copyright 2019 The Open University
+ * @author    2021 Safat Shahin <safatshahin@catalyst-au.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @deprecated since Moodle 4.0
- * @see \qbank_exporttoxml\export_xml_action_column
  */
 class export_xml_action_column extends menu_action_column_base {
+
     /** @var string avoids repeated calls to get_string('duplicate'). */
     protected $strexportasxml;
 
-    public function init() {
+    /**
+     * A chance for subclasses to initialise themselves, for example to load lang strings,
+     * without having to override the constructor.
+     */
+    public function init(): void {
         parent::init();
         $this->strexportasxml = get_string('exportasxml', 'question');
     }
 
-    public function get_name() {
+    /**
+     * Get the internal name for this column. Used as a CSS class name,
+     * and to store information about the current sort. Must match PARAM_ALPHA.
+     *
+     * @return string column name.
+     */
+    public function get_name(): string {
         return 'exportasxmlaction';
     }
 
+    /**
+     * Get the information required to display this action either as a menu item or a separate action column.
+     *
+     * If this action cannot apply to this question (e.g. because the user does not have
+     * permission, then return [null, null, null].
+     *
+     * @param \stdClass $question the row from the $question table, augmented with extra information.
+     * @return array with three elements.
+     *      $url - the URL to perform the action.
+     *      $icon - the icon for this action. E.g. 't/delete'.
+     *      $label - text label to display in the UI (either in the menu, or as a tool-tip on the icon)
+     */
     protected function get_url_icon_and_label(\stdClass $question): array {
         if (!\question_bank::is_qtype_installed($question->qtype)) {
             // It sometimes happens that people end up with junk questions
@@ -59,7 +84,9 @@ class export_xml_action_column extends menu_action_column_base {
             return [null, null, null];
         }
 
-        return [question_get_export_single_question_url($question),
+        return [exporttoxml_helper::question_get_export_single_question_url($question),
                 't/download', $this->strexportasxml];
     }
+
 }
+
