8ef83e388916a128126b0813b57aaea138c07761
Safat Shahin <safatshahin@catalyst-au.net>
2021-06-07 19:17:06 +1000

    MDL-71641 qbank_tagquestion: Add Tag question plugin to core
    This implementation will introduce a qbank plugin "tagquestion"
    which will implement the tag question actions in the question
    bank view by replacing the core class. Having this plugin
    will give users the flexibility of enabling or
    disabling this action.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,7 @@ class core_plugin_manager {
             ),
 
             'qbank' => array(
-                'comment', 'previewquestion'
+                'comment', 'previewquestion', 'tagquestion',
             ),
 
             'qbehaviour' => array(
