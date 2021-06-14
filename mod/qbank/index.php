f55c5f676ff21812342b8827a8227fba1321306c
Guillermo Gomez <guillermogomez@catalyst-au.net>
2021-06-14 16:13:02 +1000

    Fix index page mod_qbank

--- mod/qbank/index.php
+++ mod/qbank/index.php
@@ -24,68 +24,63 @@
  */
 
 require(__DIR__.'/../../config.php');
-
 require_once(__DIR__.'/lib.php');
 
 $id = required_param('id', PARAM_INT);
 
-$course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
+$course = $DB->get_record('course', ['id' => $id], '*', MUST_EXIST);
 require_course_login($course);
 
 $coursecontext = context_course::instance($course->id);
 
-$event = \mod_qbank\event\course_module_instance_list_viewed::create(array(
-    'context' => $modulecontext
-));
+$event = \mod_qbank\event\course_module_instance_list_viewed::create(['context' => $coursecontext]);
 $event->add_record_snapshot('course', $course);
 $event->trigger();
 
-$PAGE->set_url('/mod/qbank/index.php', array('id' => $id));
+$PAGE->set_url('/mod/qbank/index.php', ['id' => $id]);
 $PAGE->set_title(format_string($course->fullname));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($coursecontext);
 
-echo $OUTPUT->header();
-
 $modulenameplural = get_string('modulenameplural', 'mod_qbank');
+echo $OUTPUT->header();
 echo $OUTPUT->heading($modulenameplural);
 
 $qbanks = get_all_instances_in_course('qbank', $course);
 
 if (empty($qbanks)) {
-    notice(get_string('no$qbankinstances', 'mod_qbank'), new moodle_url('/course/view.php', array('id' => $course->id)));
+    notice(get_string('thereareno', 'moodle', $modulenameplural), new moodle_url('/course/view.php', ['id' => $course->id]));
 }
 
 $table = new html_table();
 $table->attributes['class'] = 'generaltable mod_index';
 
+$align = ['center', 'left'];
 if ($course->format == 'weeks') {
-    $table->head  = array(get_string('week'), get_string('name'));
-    $table->align = array('center', 'left');
+    $table->head  = [get_string('week'), get_string('name')];
+    $table->align = ['center', 'left'];
 } else if ($course->format == 'topics') {
-    $table->head  = array(get_string('topic'), get_string('name'));
-    $table->align = array('center', 'left', 'left', 'left');
+    $table->head  = [get_string('topic'), get_string('name')];
+    $table->align = ['center', 'left'];
 } else {
-    $table->head  = array(get_string('name'));
-    $table->align = array('left', 'left', 'left');
+    $table->head  = [get_string('name')];
+    $table->align = ['left'];
 }
 
 foreach ($qbanks as $qbank) {
+    $attributes = [];
     if (!$qbank->visible) {
-        $link = html_writer::link(
-            new moodle_url('/mod/qbank/view.php', array('id' => $qbank->coursemodule)),
-            format_string($qbank->name, true),
-            array('class' => 'dimmed'));
-    } else {
-        $link = html_writer::link(
-            new moodle_url('/mod/qbank/view.php', array('id' => $qbank->coursemodule)),
-            format_string($qbank->name, true));
+        $attributes['class'] = 'dimmed';
     }
+    $link = html_writer::link(
+        new moodle_url('/mod/qbank/view.php', ['id' => $qbank->coursemodule]),
+        format_string($qbank->name, true),
+        $attributes);
 
     if ($course->format == 'weeks' or $course->format == 'topics') {
-        $table->data[] = array($qbank->section, $link);
+        $table->data[] = [$qbank->section, $link];
     } else {
-        $table->data[] = array($link);
+        $table->data[] = [$link];
     }
 }
 
