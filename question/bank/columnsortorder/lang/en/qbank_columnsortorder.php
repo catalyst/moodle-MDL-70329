e00fec26607061efa3f82a9a0f5597d33ae7af9f
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-06-15 09:54:56 -0400

    MDL-71636 qbank_columnsortorder: Add a columnsortorder settings page
    This implementation will introduce a feature "columnsortorder"
    which will add the column sort order feature in an external page.
    Having this feature will give users the flexibility of sorting plugin
    columns in the question bank view.

---
+++ question/bank/columnsortorder/lang/en/qbank_columnsortorder.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component qbank_columnsortorder, language 'en'.
+ *
+ * @package    qbank_columnsortorder
+ * @copyright  2021 Catalyst IT Australia Pty Ltd
+ * @author     Ghaly Marc-Alexandre <marc-alexandreghaly@catalyst-ca.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Column sort order';
+$string['privacy:metadata'] = 'Column sort order does not store any personal data.';
+// Used in admin settings page.
+$string['qbankcolumnsortorder'] = 'Column sort order';
+// When plugin is enabled, caption is present in manage qbank page.
+$string['qbankgotocolumnsort'] = 'You can reorder column order in the question bank view on the {$a} page';
+// Caption present in column sort order external page.
+$string['qbankcolumnsdisabled'] = 'Columns below are currently disabled.';
+$string['qbankgotomanageqbanks'] = 'Enable and disable column plugins via <a href=\'{$a}\'>Manage question bank plugins</a>';
+$string['qbankcolumnname'] = '({$a})';
+$string['qbanksortdescription'] = 'Column order modification in this page will reorder column display in question bank view';
