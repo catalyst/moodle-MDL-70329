f9ea04a37ea742041824799bbe1a75e8143ed3c1
Safat Shahin <safatshahin@catalyst-au.net>
2021-10-25 00:17:11 +1100

    MDL-71696 core_question: Implement question status change
    This commit implements the status change pop up for a question
    in the base view. This feature update will allow the change of
    status of a question without creating a new version.
    This commit also implements the status as a char rather than
    an int value.

--- question/classes/statistics/questions/calculated.php
+++ question/classes/statistics/questions/calculated.php
@@ -228,7 +228,6 @@ class calculated {
         foreach ($this->fieldsindb as $field) {
             $toinsert->{$field} = $this->{$field};
         }
-        //var_dump($this->facility);die;
         $DB->insert_record('question_statistics', $toinsert, false);
 
         if ($this->get_variants()) {
