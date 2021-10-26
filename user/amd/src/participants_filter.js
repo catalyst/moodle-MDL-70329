6ed2af62799bb29b913af30852cb34c46b9ac7a0
Tomo Tsuyuki <tomotsuyuki@catalyst-au.net>
2021-10-26 16:08:27 +1100

    MDL-72509 core: Move participants filter from core_user to core

---
+++ user/amd/src/participants_filter.js
@@ -0,0 +1,34 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Participants filter managemnet.
+ *
+ * @module     core_user/participants_filter
+ * @copyright  2021 Tomo Tsuyuki <tomotsuyuki@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import * as CoreFilter from 'core/filter';
+
+/**
+ * Initialise the participants filter on the element with the given id.
+ *
+ * @param {String} filterRegionId
+ */
+export const init = filterRegionId => {
+    CoreFilter.init(filterRegionId, 'DynamicTable');
+};
+
