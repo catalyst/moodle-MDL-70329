2304e64df18334aafc3388ff90eaaab7f977c5b1
Safat Shahin <safatshahin@catalyst-au.net>
2021-10-28 16:07:53 +1100

    MDL-71696 core_question: peer review change for status
    This commit implements the status as a char rather than
    a int value. This came as a part of peer review of the
    versioning.

--- question/bank/editquestion/version.php
+++ question/bank/editquestion/version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbank_editquestion';
-$plugin->version   = 2021102500;
+$plugin->version   = 2021102800;
 $plugin->requires  = 2021052500;
 $plugin->maturity  = MATURITY_STABLE;
